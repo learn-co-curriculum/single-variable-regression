ddb0732be1f842d15f318e6c072f7eddcf1dfce2
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-02-16 16:33:09 -0500

    update to use the library

---
+++ graph.py
@@ -0,0 +1,22 @@
+import plotly
+from plotly.offline import iplot, init_notebook_mode
+init_notebook_mode(connected=True)
+
+def trace(data, mode = 'markers', name="data"):
+    x_values = list(map(lambda point: point['x'],data))
+    y_values = list(map(lambda point: point['y'],data))
+    return {'x': x_values, 'y': y_values, 'mode': mode, 'name': name}
+
+def trace_values(x_values, y_values, mode = 'markers', name="data", text_values = []):
+    return {'x': x_values, 'y': y_values, 'mode': mode, 'name': name, 'text': text_values}
+
+def layout(x_range = None, y_range = None, options = {}):
+    layout = {}
+    if isinstance(x_range, list): layout.update({'xaxis': {'range': x_range}})
+    if isinstance(y_range, list): layout.update({'yaxis': {'range': y_range}})
+    layout.update(options)
+    return layout
+
+def plot(traces = [], layout = {}):
+    if not isinstance(traces, list): raise TypeError('first argument must be a list.  Instead is', traces)
+    plotly.offline.iplot({'data': traces, 'layout': layout})
